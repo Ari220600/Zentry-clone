cf450d3462ff5fa9741e2559d01bd0e897b26dc6
Ari220600 <arindamchakraborty2k@gmail.com>
2024-12-01 02:07:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import "./index.css";
 import About from "./components/About";
 import Features from "./components/Features";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Contact from "./components/Contact";
 import ThreeSectionScroll from "./components/ThreeSectionScroll";
 import Footer from "./components/Footer";
